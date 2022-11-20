e90077c2a2413fa113c02917436b361ad0725c9c
이윤기 <ahacross@naver.com>
2022-11-20 23:03:25 +0900

    og2 - 이미지 변경

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,56 @@
       href="https://developers.kakao.com/tool/debugger/sharing"
       target="_blank"
     >
-      카카오 테스트 페이지
+      카카오 OG 캐시 삭제 사이트
     </a>
+    <br />
+    <br />
+    <button @click="shareKakao">카카오 공유하기</button>
   </main>
 </template>
+<script>
+export default {
+  methods: {
+    shareKakao() {
+      window.Kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: '딸기 치즈 케익',
+          description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
+          imageUrl:
+            'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          link: {
+            // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+        social: {
+          likeCount: 286,
+          commentCount: 45,
+          sharedCount: 845,
+        },
+        buttons: [
+          {
+            title: '네이버로 보기',
+            link: {
+              mobileWebUrl: 'https://naver.com',
+              webUrl: 'https://naver.com',
+            },
+          },
+          {
+            title: '앱으로 보기',
+            link: {
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+        ],
+      });
+    },
+  },
+};
+</script>
 
 <style scoped>
 header {
